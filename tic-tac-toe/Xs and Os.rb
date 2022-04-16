5856291fc57070f4b32804d5f363b983414cdde7
Ose-Augustine <osebeckley@gmail.com>
2022-04-16 10:43:02 +0100

    Added empty_boxes? predicate method to check if boxes on board are still emtpy.

--- tic-tac-toe/Xs and Os.rb
+++ tic-tac-toe/Xs and Os.rb
@@ -2,12 +2,18 @@ class TicTacToe
     @@current_players=0
     def initialize(name)
         @name = name
+        @@current_players += 1
         
     end
-    def generateBoard
+    def generate_board
         board=Array.new(3) {Array.new(3)}
-        board.each {|element| p element}
+        board
     end 
+    def empty_boxes?
+        board= generate_board
+        p board.any? {|element| element.include?(nil)}
+    end
 end
 player=TicTacToe.new("emeka")
-player.generateBoard
+player.generate_board
+player.empty_boxes?
