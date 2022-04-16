3e107cd13a4e1b3c5843b528bb960aa55bfa9873
Ose-Augustine <osebeckley@gmail.com>
2022-04-16 11:03:38 +0100

    Created New child class for players.
    Created function (populate board) to populate the current playing board with the players choice

--- tic-tac-toe/Xs and Os.rb
+++ tic-tac-toe/Xs and Os.rb
@@ -1,5 +1,6 @@
 class TicTacToe
     @@current_players=0
+    attr_reader :name
     def initialize(name)
         @name = name
         @@current_players += 1
@@ -14,6 +15,19 @@ class TicTacToe
         p board.any? {|element| element.include?(nil)}
     end
 end
+class Human < TicTacToe
+    def populate_board(play_choice,row_position,column_position)
+        board=generate_board
+        board[row_position][column_position]=play_choice
+        p board
+    end 
+end
 player=TicTacToe.new("emeka")
 player.generate_board
 player.empty_boxes?
+p player.name
+person=Human.new('james')
+person2=Human.new('michael')
+person.populate_board('x',0,0)
+person2.populate_board('O',0,1)
+p person.name
