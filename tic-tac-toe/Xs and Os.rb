285df5d39575e638d97002dcde0039bbbe935d46
Ose-Augustine <osebeckley@gmail.com>
2022-04-17 16:13:21 +0100

    Created function to count current_players

--- tic-tac-toe/Xs and Os.rb
+++ tic-tac-toe/Xs and Os.rb
@@ -1,6 +1,7 @@
 class TicTacToe
     @@current_players=0
-    attr_reader :name
+    attr_reader :name 
+    attr_reader :current_players
     def initialize(name)
         @name = name
         @@current_players += 1
@@ -12,22 +13,16 @@ class TicTacToe
     end 
     def empty_boxes?
         board= generate_board
-        p board.any? {|element| element.include?(nil)}
+        board.any? {|element| element.include?(nil)}
+    end
+    def self.player_count
+        return @@current_players
     end
 end
 class Human < TicTacToe
     def populate_board(play_choice,row_position,column_position)
         board=generate_board
         board[row_position][column_position]=play_choice
-        p board
+        board
     end 
 end
-player=TicTacToe.new("emeka")
-player.generate_board
-player.empty_boxes?
-p player.name
-person=Human.new('james')
-person2=Human.new('michael')
-person.populate_board('x',0,0)
-person2.populate_board('O',0,1)
-p person.name
