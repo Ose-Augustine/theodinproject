16028090e5abbfccaf7b3b219ec670f4da725c02
Ose-Augustine <osebeckley@gmail.com>
2022-04-17 17:15:46 +0100

    Added board as a class variable rather than an instance method
    Generate board now a function for class rather than instance.

--- tic-tac-toe/Xs and Os.rb
+++ tic-tac-toe/Xs and Os.rb
@@ -1,28 +1,29 @@
+
 class TicTacToe
     @@current_players=0
     attr_reader :name 
-    attr_reader :current_players
+    @@board=Array.new(3) {Array.new(3)}
     def initialize(name)
         @name = name
         @@current_players += 1
-        
     end
-    def generate_board
-        board=Array.new(3) {Array.new(3)}
-        board
-    end 
+    def self.generate_board
+        return @@board
+    end
     def empty_boxes?
-        board= generate_board
-        board.any? {|element| element.include?(nil)}
+        @@board.any? {|element| element.include?(nil)}
     end
     def self.player_count
         return @@current_players
     end
+    def populate_board(player_choice,row_position,column_position)
+        @@board[row_position][column_position]=player_choice
+        @@board
+    end
 end
-class Human < TicTacToe
-    def populate_board(play_choice,row_position,column_position)
-        board=generate_board
-        board[row_position][column_position]=play_choice
-        board
-    end 
-end
+
+p TicTacToe.generate_board
+player1=TicTacToe.new("amaka")
+p TicTacToe.player_count
+p player1.populate_board('x',0,0)
+p player1.empty_boxes?
