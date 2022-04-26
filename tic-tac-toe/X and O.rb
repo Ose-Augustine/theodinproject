ececf30bc83aebed9f715cc45d9c356590861fc5
Ose-Augustine <osebeckley@gmail.com>
2022-04-26 21:46:24 +0100

    Removed the prompts for player names in the umpire class
    New class player doesn't need to store the identity of the players.
    Removed initailise for umpire class and player class.
    Once umpire is intantiated the player class would need to be also to get the game going.

--- tic-tac-toe/X and O.rb
+++ tic-tac-toe/X and O.rb
@@ -1,26 +1,41 @@
 
 class Umpire
-    p "Register Player one"
-    @@player1, choice1=gets.chomp , gets.chomp#get both the player name and what the player prefers to use
-    p"Register Player two"
-    @@player2, choice2=gets.chomp , gets.chomp
-    def initialize (name)
-        @name=name
-    end
-    def generate_board
+    def self.generate_board
         board= Array.new(3) {Array.new(3)}
-        p board 
+        board 
     end 
-    def populate_board(player_choice,row_position,column_position)
-        board=generate_board
-        board[row_position][column_position]=player_choice
-        board
-    end
     def self.current_players
         return @@player1 , @@player2
     end
+    def winner?
+        board=generate_board
+        possible_win_1=[0,1,2]
+        possible_win_2=[0,0,0]
+        possible_win_3=[1,1,1]
+        possible_win_4=[2,2,2]
+        pattern=[]
+        pattern2=[]
+        board.each |element| 
+            element.each_with_index {|play,index|pattern.push(index) if play=='x'}
+        pattern 
+    end
 end
-umpire=Umpire.new("michael")
-umpire.generate_board
-p Umpire.current_players
-p umpire.populate_board('x',0,0)
+class Player < Umpire
+    @@board=Umpire.generate_board
+    def empty_boxes?
+        @@board.any? {|element| element.include?(nil)}
+    end
+    def populate_board(player_choice,row_position,column_position)
+        @@board[row_position][column_position]=player_choice
+        @@board
+    end
+end
+
+umpire=Umpire.new
+player=Player.new
+p player.populate_board('x',0,0)
+p player.populate_board('o',0,1)
+# new_group=Umpire.new("james")
+# new_group.name
+# p Umpire.current_players
+# p umpire.populate_board('x',0,0)
