e509350a52817a363e18c4721921c0745ffb5c32
Ose-Augustine <osebeckley@gmail.com>
2022-04-26 13:59:58 +0100

    Added new methods
    Generate board method to generate the board for the game.
    populate board method to input the player choice at a particular location of the board
    current players to show the players that are currently on .

--- tic-tac-toe/X and O.rb
+++ tic-tac-toe/X and O.rb
@@ -1,14 +1,26 @@
 
 class Umpire
     p "Register Player one"
-    @@player1=gets.chomp
-    p "Register Player two"
-    @@player2=gets.chomp
+    @@player1, choice1=gets.chomp , gets.chomp#get both the player name and what the player prefers to use
+    p"Register Player two"
+    @@player2, choice2=gets.chomp , gets.chomp
     def initialize (name)
         @name=name
     end
-    def player_identity
-        player1=gets.chomp
+    def generate_board
+        board= Array.new(3) {Array.new(3)}
+        p board 
+    end 
+    def populate_board(player_choice,row_position,column_position)
+        board=generate_board
+        board[row_position][column_position]=player_choice
+        board
+    end
+    def self.current_players
+        return @@player1 , @@player2
     end
 end
 umpire=Umpire.new("michael")
+umpire.generate_board
+p Umpire.current_players
+p umpire.populate_board('x',0,0)
