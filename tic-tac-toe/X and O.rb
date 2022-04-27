0faaba026959edf48b6e8c6d53987c341adcbc7e
Ose-Augustine <osebeckley@gmail.com>
2022-04-27 09:51:32 +0100

    Added some new functionalities
    Changed all methods to class methods rather than instance methods because of some bug issues in calling functions from parent class.
    Created the winner method in umpire class to check for a winner using only 'x' currently.
    Line 29 looks for the index position of string(x) before matching against win_board on line 30.
    Updated while loop in the play class to stop game once a winner is found.

--- tic-tac-toe/X and O.rb
+++ tic-tac-toe/X and O.rb
@@ -1,44 +1,57 @@
 
 class Umpire
-    def self.generate_board
+    def initialize(name)
+        @name=name
+    end
+    def generate_board
         board= Array.new(3) {Array.new(3)}
         board 
     end 
     def self.current_players
         return @@player1 , @@player2
     end
-    def winner?
-        board=generate_board
-        possible_win_1=[0,1,2]
-        possible_win_2=[0,0,0]
-        possible_win_3=[1,1,1]
-        possible_win_4=[2,2,2]
-        pattern=[]
-        pattern2=[]
-        board.each |element| 
-            element.each_with_index {|play,index|pattern.push(index) if play=='x'}
-        pattern 
+    def self.empty_boxes?(board)
+        board.any? {|element| element.include?(nil)}
+    end
+    def self.winner?(board)
+        vertical_1=[0,3,6]
+        vertical_2=[1,4,7]
+        vertical_3=[2,5,8]
+        diagonal_1=[0,4,8]
+        diagonal_2=[2,4,6]
+        horizontal_1=[0,1,2]
+        horizontal_2=[3,4,5]
+        horizontal_3=[6,7,8]
+        win_board=[vertical_1,vertical_2,vertical_3,diagonal_1,diagonal_2,horizontal_1,horizontal_2,horizontal_3]
+        accumulator=''
+        win_combination=[]
+        board.each {|nested| accumulator += nested.join}
+        accumulator.length.times {|index| win_combination << index if accumulator[index,1]=='x'}
+        win_board.any? {|element| element==win_combination}
+
     end
 end
 class Play < Umpire
-    @@board=Umpire.generate_board
-    def self.empty_boxes?
-        @@board.any? {|element| element.include?(nil)}
-    end
+    board=generate_board
+   
     def self.populate_board(player_choice,row_position,column_position)
         @@board[row_position][column_position]=player_choice
         @@board
     end
-    while Play.empty_boxes? == true
+    while Umpire.empty_boxes?(@@board) == true
         player_choice=gets.chomp
         row_position=gets.chomp.to_i
         column_position=gets.chomp.to_i
         p Play.populate_board(player_choice,row_position,column_position)
+        if Umpire.winner?(@@board)==true then 
+            p "Player one wins "
+            break
+        end
     end
 end
 
-umpire=Umpire.new
-player=Play.new
+umpire=Umpire.new('james')
+player=Play.new('amka')
 # new_group=Umpire.new("james")
 # new_group.name
 # p Umpire.current_players
