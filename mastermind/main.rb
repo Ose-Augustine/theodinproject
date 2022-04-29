e59d912e23e5bf0c4d53964245667175d817a6d0
Ose-Augustine <osebeckley@gmail.com>
2022-04-29 16:26:14 +0100

    Changed the Mastermind class to module
    The CodeBreaker could not inherit the play board from the mastermind module and the color board at the same time.
    The colour board had a bug of reseting itself every time a round is played hence the move the the mastermind module.
    The fix sets just one color board and play board for the game.

--- mastermind/main.rb
+++ mastermind/main.rb
@@ -1,37 +1,42 @@
-class Mastermind
-    @@play_board=Array.new(9) {Array.new(4,0)}
-    def display
-        @@play_board
-    end
+module Mastermind
+    PLAY_AREA=Array.new(9) {Array.new(4,0)}
+    COLORS=['red','orange','white','purple','yellow']
+    SELCTED_COLORS=COLORS.sample(4)#return any random 4 elements from array
 end
 
-class CodeMaker < Mastermind
-    COLORS=['red','orange','white','purple','yellow']
+class CodeMaker
+    include Mastermind
+    @@play_board=PLAY_AREA
+   
     def initialize(name)
         @name=name
     end
-    def random_from_colors
-        code_maker_colors=COLORS.sample(4)#return any random 4 elements from array
-    end
     def check_mastermind
-        chosen_colors=random_from_colors
+        chosen_colors=SELCTED_COLORS
         check_board=@@play_board.select {|row| !row.include?(0)}
+        round=0
         check_board.each do |colour_row|
-            round=0
             red_tag=0
             white_tag=0
             colour_row.each_with_index do |colour,position| 
                 if colour==chosen_colors[position] then 
                     red_tag+=1
+                end
                 if chosen_colors.include?(colour) && colour != chosen_colors[position]
                     white_tag+=1
                 end
-                round+=1
-            p "You got #{red_tag} and #{white_tag} for this round: #{round}."
+            end
+            round += 1
+            #winning condition
+            if red_tag==4 then
+                p "CodeBreaker won this round"
+                break 
+            end
+            p "You got #{red_tag} red tags and #{white_tag} white tags  for this round: #{round}"
         end
     end
 end
-class CodeBreaker
+class CodeBreaker < CodeMaker
     def initialize(name)
         @name=name
     end
@@ -42,13 +47,22 @@ class CodeBreaker
             guess=gets.chomp
             guess_combination.push(guess)
         end
+        @@play_board[row_position]=guess_combination
     end
-    
+    def start_game
+        p "The board for this game:"
+        p @@play_board
+        until @@play_board.any? {|row| row.include?(0)}==false
+            p "What row position"
+            position=gets.chomp.to_i
+            populate_board(position)
+            check_mastermind
+            p @@play_board
+        end
+    end
 end
 
 #tests
 player=CodeMaker.new('emeka')
-game=Mastermind.new
-p player.check_mastermind
-p game.display
-p player.random_from_colors
+player2=CodeBreaker.new('john')
+player2.start_game
