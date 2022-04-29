2e9d8b321601234d66e656396aca2b2de89d0485
Ose-Augustine <osebeckley@gmail.com>
2022-04-29 13:37:20 +0100

    Created the mastermind class for general rules

--- mastermind/main.rb
+++ mastermind/main.rb
@@ -1,9 +1,17 @@
-class CodeMaker
+class Mastermind
+    play_board=Array.new(9) {Array.new(4)}
+end
+class CodeMaker < Mastermind
     COLORS=['red','orange','white','purple','yellow']
-    def initialise(name)
+    def initialize(name)
         @name=name
     end
     def random_from_colors
-        code_maker_colors=COLORS.sample(4)
+        code_maker_colors=COLORS.sample(4)#return any random 4 elements from array
+    end
+    def check_mastermind
     end
 end
+
+player=CodeMaker.new('emeka')
+p player.random_from_colors
