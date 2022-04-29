0e22f37096e9e2219f5f29c0a3fd1304e66f6ed8
Ose-Augustine <osebeckley@gmail.com>
2022-04-29 14:42:46 +0100

    Updated the check_mastermind with specific rules on hwo to check the board-m Not yet tested if working as expected. Take note of the ! marks that negate the conditions on lines 18 and 26.

--- mastermind/main.rb
+++ mastermind/main.rb
@@ -14,11 +14,24 @@ class CodeMaker < Mastermind
         code_maker_colors=COLORS.sample(4)#return any random 4 elements from array
     end
     def check_mastermind
+        chosen_colors=random_from_colors
         check_board=@@play_board.select {|row| !row.include?(0)}
-        check_board
+        check_board.each do |colour_row|
+            round=0
+            red_tag=0
+            white_tag=0
+            colour_row.each_with_index do |colour,position| 
+                if colour==chosen_colors[position] then 
+                    red_tag+=1
+                if chosen_colors.include?(colour) && colour != chosen_colors[position]
+                    white_tag+=1
+                end
+                round+=1
+            p "You got #{red_tag} and #{white_tag} for this round: #{round}."
+        end
     end
 end
-
+#tests
 player=CodeMaker.new('emeka')
 game=Mastermind.new
 p player.check_mastermind
