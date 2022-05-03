7bbcbf6ae5d2dbdd6b66e9922a826751b49c2e66
Ose-Augustine <osebeckley@gmail.com>
2022-05-03 16:00:50 +0100

    New additions to computer algorithm
    Added parameter to the examine pegs so that when it is called the first time in start game, it uses POSSIBLE CODES and then the filtered list subsequelty in the until loop.

--- mastermind/computer_algorithm.rb
+++ mastermind/computer_algorithm.rb
@@ -1,30 +1,33 @@
 require_relative 'human_feedback.rb'
 include HumanFeedback
+require 'pry-byebug'
 module ComputerAlgorithm 
     POSSIBLE_CODES=(1111..6666).to_a
+    def selected_codes(filters)
+        filters
+    end
     def computer_to_play_initially
         computer_initial_choice=1122
     end
     def knuths_algorithm(filtered_codes,feedback)
     
     end
-
-
-    def examine_pegs(feedback,play)
+    def examine_pegs(chosen_codes,feedback,play)
         selected_codes=[]
         play=play.to_s.split('')
-        POSSIBLE_CODES.each do |code|
-            qualifying_feedback=[0,0]
+        chosen_codes.each do |code|
+            red_peg=0
+            white_peg=0
             check_form=code.to_s.split('')
             check_form.each_with_index do |digit,position|
                 if digit==play[position] 
-                    qualifying_feedback[0] += 1
+                    red_peg += 1
                 end
                 if play.include?(digit) && digit != play[position]
-                    qualifying_feedback[1] += 1
+                    white_peg += 1
                 end
             end
-            if qualifying_feedback.eql?(feedback)
+            if [red_peg,white_peg].eql?(feedback)
                 selected_codes.push(code)
             end
         end
@@ -43,16 +46,18 @@ module ComputerAlgorithm
     def computer_subsequent_turns
         round=2
         current_feedback=computer_start_game
-        chosen_codes=examine_pegs(current_feedback,computer_to_play_initially)
+        chosen_codes=examine_pegs(POSSIBLE_CODES,current_feedback,computer_to_play_initially)
         round += 1
         chosen_code=chosen_codes[0]
         until current_feedback[0]==4
             p chosen_code
             current_feedback=HumanFeedback.human_feedback
-            filtered_codes=examine_pegs(current_feedback,chosen_code)
+            p filtered_codes=examine_pegs(chosen_codes,current_feedback,chosen_code)
+            chosen_codes=filtered_codes
             chosen_code=current_code_guess(filtered_codes)
         end
     end
 end
+#ComputerAlgorithm.examine_pegs([2,1],1122)
 
 
