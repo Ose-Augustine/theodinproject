7eadaf83d30cdcfc0413de770b06f7bf5deae322
Ose-Augustine <osebeckley@gmail.com>
2022-05-09 19:32:36 +0100

    Created new class for Gamer controls and removed initialise from hangman controls
    The hangman controls class will be the umpire and so once the game is set to start , it will auto initialise.

--- hangman/main.rb
+++ hangman/main.rb
@@ -1,20 +1,27 @@
 class HangManControls
+    def dashboard_for_guessing
+        word = extract_random_guess_word
+        board = Array.new(word.length,'-')
+        p word
+        board 
+    end
+
+    def required_length?(word)
+        word.length >= 5 || word.length <= 20
+    end
+
+    def extract_random_guess_word
+        wordslist      = File.open('hangman_guess_words.txt')
+        lines          = wordslist.readlines
+        selected_words = lines.select {|word| required_length?(word)}
+        selected_words.sample.chomp
+    end
+end
+class Gamer < HangmanControls 
     attr_reader :name
     def initialize(name)
         @name = name
     end
-
-    def dashboard_for_guessing;end
-    def required_length?(word)
-        word.length >= 5 || word.length <= 20
-    end
-    def extract_random_guess_word
-        wordslist = File.open('hangman_guess_words.txt')
-        lines     = wordslist.readlines
-        selected_words = lines.select {|word| required_length?(word)}
-        selected_words.sample.chomp
-    end
-
 end
 player1 = HangManControls.new('emeka')
-p player1.extract_random_guess_word
+p player1.dashboard_for_guessing
